21e291815760d939e275aa00ca2be0fd255515bb
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-12-30 16:27:28 +0530

    added Pattern Problem - 5 & 6

--- DSA/PatternProb/pattern5.cpp
+++ DSA/PatternProb/pattern5.cpp
@@ -11,11 +11,22 @@ void pattern5(int n){
 }
 }
 
+void pattern6(int n){
+  for(int i=1; i<=n; i++){
+    for(int j=1; j<=n-i+1; j++){
+      cout << j << " ";
+    }
+    cout << endl;
+}
+}
 int main(){
   int n;
   cout << "Enter the number of rows: ";
   cin >> n;
 
   pattern5(n);
+  pattern6(n);
 
 }
+
+
