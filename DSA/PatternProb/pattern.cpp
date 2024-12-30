3c6d935d151127166c6150152a274aa9d1f1653f
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2024-12-30 15:19:23 +0530

    added Pattern Problem - 1

---
+++ DSA/PatternProb/pattern.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printPattern(){
+  for (int i=0; i<4 ; i++){
+    for (int j =0; j<4 ; j++){
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+int main(){
+  printPattern();
+}
