8c1a0c8b9a91062cc07acb0a3910f137fdc43953
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2025-01-13 19:49:06 +0530

    Recursion: Print name N times using recursion

---
+++ DSA/Recursion/Question2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void printGfg(int N){
+  if(N==0) return;
+
+  printGfg(N-1);
+  cout<<"GFG" << " "; 
+}
+
+int main(){
+  int N;
+  cin>>N;
+  printGfg(N);
+  return 0;
+}
+
